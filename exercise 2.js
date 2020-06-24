0b29a07246f140ecafd480f6446b3a6e04d8350e
Антонов Андрей <an_42@mail.ru>
2020-06-24 17:27:17 +0700

    добавлено задание 2

---
+++ exercise 2.js
@@ -0,0 +1,69 @@
+const findValueByPath = (item, path) => {
+    return path.split('.').reduce((prev, cur, index, array) => {
+        return prev[cur] ? prev[cur] : item;
+    },item);
+};
+
+const autocompleteObjectNode = (origin) => {
+  const proxyWrapper = (object, path) => {
+      return new Proxy(object, {
+          set: (obj, prop, value) => {
+              console.log({
+                  method: "set",
+                  current: obj,
+                  prop,
+                  propType: typeof prop,
+                  value,
+                  origin,
+                  path
+              });
+              const item = findValueByPath(origin, path);
+              Object.defineProperty(item, prop, {value: value, writable: true, configurable: true, enumerable: true});
+              return proxyWrapper(item, `${path}.${prop}`);
+          },
+          get: (obj, prop) => {
+              console.log({
+                  method: "get",
+                  current: obj,
+                  prop,
+                  propType: typeof prop,
+                  origin,
+                  path
+              });
+              if (obj[prop] === typeof "object") {
+                  //console.log("getting prop is exist and object");
+                  return proxyWrapper({...obj[prop]}, `${path}.${prop}`);
+              } else {
+                  if (!obj[prop]
+                      && typeof prop !== "symbol"
+                      && prop !== "inspect"
+                  ) {
+                      //console.log("getting prop not exist");
+                      const item = findValueByPath(origin, path);
+                      Object.defineProperty(item, prop, {value: {}, writable: true, configurable: true, enumerable: true});
+                      return proxyWrapper({}, `${path}.${prop}`);
+                  } else {
+                      //console.log("getting prop is exist and not object");
+                      return Reflect.get(obj, prop);
+                  }
+              }
+          }
+      });
+  };
+
+  return proxyWrapper(origin, "");
+};
+
+const obj = {
+    "first": "10",
+    "second": "20",
+    "extra": {
+        "subextra": 0
+    }
+};
+
+const proxiedObj = autocompleteObjectNode(obj);
+
+proxiedObj.third.fourty.fifty = "110";
+
+console.log("result", proxiedObj);
