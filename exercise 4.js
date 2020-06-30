fafbe0e318680d75efde337adbab9fbd09aea087
Антонов Андрей <an_42@mail.ru>
2020-06-30 20:33:04 +0700

    оптимизация рендера: memo, callback, key

--- exercise 4.js
+++ exercise 4.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const SimpleComponent = ({ number, componentRerenderedTimes }) => {
+const SimpleComponent = React.memo(({ number, componentRerenderedTimes }) => {
     componentRerenderedTimes.current++;
 
     const onPress = () => alert(number);
 
-    return <div onClick={() => onPress()}>Number: {number}</div>;
-};
+    return <div onClick={onPress}>Number: {number}</div>;
+});
 
 export default function App() {
     const componentRerenderedTimes = React.useRef(0);
@@ -16,24 +16,26 @@ export default function App() {
             .map((item, index) => ({ number: item.number, id: String(index + 1) }))
     );
 
-    const random = () =>
+    const random = () => {
         setData(
             data.map(({ id }) => ({ number: Math.floor(1 + Math.random() * 10), id }))
         );
+    };
+
+    const addToTop = () => {
+        setData(
+            data => [{ number: 0, id: Math.random() }, ...data]
+        );
+    };
 
     return (
         <div>
             <div>Was rendered: {componentRerenderedTimes.current}</div>
-            <button onClick={() => random()}>random</button>
-            <button
-                onClick={() =>
-                    setData(data => [{ number: 0, id: Math.random() }, ...data])
-                }
-            >
-                add to top
-            </button>
-            {data.map(item => (
+            <button onClick={random}>random</button>
+            <button onClick={addToTop}>add to top</button>
+            {data.map((item) => (
                 <SimpleComponent
+                    key={item.id}
                     number={item.number}
                     componentRerenderedTimes={componentRerenderedTimes}
                 />
